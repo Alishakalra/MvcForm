662a054921d03a8a8d3bb7c7cbfed643c0670792
AlishaKalra <AlishaKalra@HSSLPT-673>
2021-06-04 18:11:58 +0530

    Add project files.

---
+++ Controllers/UserInfoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        MvcFormDBEntities1 dbObj = new MvcFormDBEntities1();
+        // GET: Home
+        public ActionResult UserInfo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddInfo(UserInfoTable model)
+        {
+            UserInfoTable info = new UserInfoTable();
+            if (ModelState.IsValid)
+            {
+                info.firstname = model.firstname;
+                info.lastname = model.lastname;
+                info.email = model.email;
+
+                dbObj.UserInfoTables.Add(info);
+                dbObj.SaveChanges();
+                return View();
+
+
+            }
+
+            return View("UserInfo");
+
+            
+            
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Web;
+using System.Linq;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Enter valid firstname")]
+        public string firstname { get; set; }
+
+        [Required(ErrorMessage = "Enter valid lasname")]
+        public string lastname { get; set; }
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+        [Required]
+        public int age { get; set; }
+
+    }
+}
